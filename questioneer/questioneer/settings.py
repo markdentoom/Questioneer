2025a988d84dc2252c3249cebbda7fbad475fb85
markdentoom <markdentoom@hotmail.com>
2022-02-17 11:11:49 +0100

    login and registration

--- questioneer/questioneer/settings.py
+++ questioneer/questioneer/settings.py
@@ -65,7 +65,9 @@ ROOT_URLCONF = "questioneer.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -134,3 +136,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+LOGIN_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
