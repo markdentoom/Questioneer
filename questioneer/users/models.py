09b7ab4964c0729aa9eb09d7e2042e46a1b18549
markdentoom <markdentoom@hotmail.com>
2022-02-17 10:41:06 +0100

    setup user and db

--- questioneer/users/models.py
+++ questioneer/users/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    # Use default AbstractUser, but inherit it so we can change it as we see fit later
+    pass
