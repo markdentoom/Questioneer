85dc550e289d17568266329d42d78e2e23d5b831
markdentoom <markdentoom@hotmail.com>
2022-02-17 10:30:42 +0100

    add packages

--- questioneer/questioneer/settings.py
+++ questioneer/questioneer/settings.py
@@ -31,12 +31,23 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Default django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Extra packages
+    "rest_framework",
+    "rest_framework.authtoken",
+    "django_registration",
+    "djoser",
+    "widget_tweaks",
+
+    # My apps
+    "users",
 ]
 
 MIDDLEWARE = [
